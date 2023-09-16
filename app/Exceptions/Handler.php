f27308436f2226c9375218b879f6a9a390184f32
bdiazc90 <bdiazc90@gmail.com>
2023-09-16 14:01:02 -0500

    Add Exceptions Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,9 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
     /**
      * A list of the exception types that are not reported.
      *
@@ -32,10 +32,32 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/artist/*')) { // <- Add your condition here
+                return response()->json([
+                    'message' => 'Artist not found.'
+                ], 404);
+            }
+            if ($request->is('api/album/*')) { // <- Add your condition here
+                return response()->json([
+                    'message' => 'Album not found.'
+                ], 404);
+            }
+            if ($request->is('api/song/*')) { // <- Add your condition here
+                return response()->json([
+                    'message' => 'Song not found.'
+                ], 404);
+            }
+            if ($request->is('api/search*')) { // <- Add your condition here
+                return response()->json([
+                    'message' => 'No match found.'
+                ], 404);
+            }
         });
     }
 }
